a12abff8bdd4e59da9eb294a3533216bddaf8214
Grant J. Butler <butler.grant.j@gmail.com>
2012-08-12 20:19:05 -0400

    Update .gitignore.

---
+++ Swipe/ACMRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  ACMRootViewController.h
+//  Swipe
+//
+//  Created by Grant Butler on 8/12/12.
+//  Copyright (c) 2012 ACM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ACMRootViewController : UIViewController
+
+@end
---
+++ Swipe/ACMRootViewController.m
@@ -0,0 +1,43 @@
+//
+//  ACMRootViewController.m
+//  Swipe
+//
+//  Created by Grant Butler on 8/12/12.
+//  Copyright (c) 2012 ACM. All rights reserved.
+//
+
+#import "ACMRootViewController.h"
+
+@interface ACMRootViewController ()
+
+@end
+
+@implementation ACMRootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return YES;
+}
+
+@end
